41dfff6bf9650e1f01a3d4793edb834a20d35488
“Martin <mggeorgiev@gmail.com>
2019-12-20 22:53:06 +0200

    getting the mouse position

--- mouse+keyboard/mouse-abs.py
+++ mouse+keyboard/mouse-abs.py
@@ -1,6 +1,8 @@
 import pyautogui
+pyautogui.position()
 for i in range(10):
       pyautogui.moveTo(100, 100, duration=0.25)
       pyautogui.moveTo(200, 100, duration=0.25)
       pyautogui.moveTo(200, 200, duration=0.25)
       pyautogui.moveTo(100, 200, duration=0.25)
+pyautogui.position()
