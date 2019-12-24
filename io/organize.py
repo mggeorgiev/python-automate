d36c1e292ba87b86313f05c2a2cb11648a368569
“Martin <mggeorgiev@gmail.com>
2019-12-24 09:04:40 +0200

    Add os and Filepath operations

--- io/organize.py
+++ io/organize.py
@@ -1,3 +1,6 @@
+import os
+from pathlib import Path
+
 SUBDIRECTORIES = {
     "DOCUMENTS": ['.pdf','.rtf','.txt'],
     "AUDIO": ['.m4a','.m4b','.mp3'],
@@ -10,5 +13,18 @@ def pickDirectory(value):
         for suffix in suffixes:
             if suffix == value:
                 return category
+    return 'MISC'
 
 print(pickDirectory('.pdf'))
+
+def organizeDirectory():
+    for item in os.scandir():
+        filePath = Path(item)
+        fileType = filePath.suffix.lower()
+        directory = pickDirectory(fileType)
+        directoryPath = Path(directory)
+        if directoryPath.is_dir() != True:
+            directoryPath.mkdir
+        filePath.rename(directoryPath.joinpath(filePath))
+
+organizeDirectory()
